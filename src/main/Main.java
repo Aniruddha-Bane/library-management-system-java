55482b13fd8236431e9c0748ee4c45cc70124123
Aniruddha-Bane <aniruddhabane2006@gmail.com>
2026-06-08 23:57:15 +0530

    Making changes in .gitignore

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,3 +1,29 @@
 package main;
 
-public class Main {}
+import model.Book;
+import service.LibraryService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LibraryService libraryService =
+                new LibraryService();
+
+        libraryService.addBook(
+                new Book(
+                        101,
+                        "Clean Code",
+                        "Robert Martin",
+                        5));
+
+        libraryService.addBook(
+                new Book(
+                        102,
+                        "Effective Java",
+                        "Joshua Bloch",
+                        3));
+
+        libraryService.viewBooks();
+    }
+}
--- src/service/LibraryService.java
+++ src/service/LibraryService.java
@@ -1,3 +1,83 @@
 package service;
 
-public class LibraryService {}
+import model.Book;
+
+import java.util.ArrayList;
+
+public class LibraryService {
+
+    private ArrayList<Book> books;
+
+    public LibraryService() {
+
+        books = new ArrayList<>();
+    }
+
+    /*
+     * ADD BOOK
+     */
+    public boolean addBook(Book book) {
+
+        for (Book existingBook : books) {
+
+            if (existingBook.getBookId()
+                    == book.getBookId()) {
+
+                return false;
+            }
+        }
+
+        books.add(book);
+
+        return true;
+    }
+
+    /*
+     * VIEW BOOKS
+     */
+    public void viewBooks() {
+
+        if (books.isEmpty()) {
+
+            System.out.println(
+                    "\nNo books available.");
+
+            return;
+        }
+
+        System.out.println(
+                "\n=========================");
+        System.out.println(
+                "      BOOK LIST");
+        System.out.println(
+                "=========================");
+
+        for (Book book : books) {
+
+            System.out.println(book);
+        }
+    }
+
+    /*
+     * SEARCH BOOK
+     */
+    public Book searchBook(
+            int bookId) {
+
+        for (Book book : books) {
+
+            if (book.getBookId()
+                    == bookId) {
+
+                return book;
+            }
+        }
+
+        return null;
+    }
+
+    public ArrayList<Book> getBooks() {
+
+        return books;
+    }
+}
